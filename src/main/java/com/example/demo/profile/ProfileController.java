56319a0d2a3e8e31d4b086279f74cf3e76e1ab0f
23832 <2383299053@qq.com>
2017-09-28 17:45:39 +0800

    基本信息页-表单（部分）

---
+++ src/main/java/com/example/demo/profile/ProfileController.java
@@ -0,0 +1,12 @@
+package com.example.demo.profile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+    @RequestMapping("/profile")
+    public String displayProfile() {
+        return "profile/profilePage";
+    }
+}
